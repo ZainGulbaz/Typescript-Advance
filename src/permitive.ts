11a9aa0f50caa946d1124eb6d866271d14ae6bad
Zain Gulbaz <zaingulbaz8@gmail.com>
2023-08-09 11:53:22 +0500

    Permitive data types

---
+++ src/permitive.ts
@@ -0,0 +1,25 @@
+//Permitive types in typescript
+
+//boolean
+let isPresent:boolean=false;
+
+//number
+let num:number=23.55;  //Number can contains upto 64bit/4 bytes value
+
+//string
+let nam:string="string";
+
+//undefined
+let noDefined:undefined=undefined;
+
+//null
+let isNull:null=null;
+
+//symbol
+let sym:symbol=Symbol("here");
+
+//bigint
+let biggy:bigint=20n;
+
+
+//There are sevent permitive types in typescript and every starts with small letter as they are permitive
