d79d1cd1cdf9eb59c5cc219967af132be6a49413
Zain Gulbaz <zaingulbaz8@gmail.com>
2023-08-18 13:19:04 +0500

    optional vs undefined

---
+++ src/optionalvsundefined.ts
@@ -0,0 +1,21 @@
+type Info={
+    data:string|undefined
+}
+
+
+type Info2={
+    data?:string
+}
+
+
+let info:Info={
+    data:"hello world"
+}
+info={}
+
+
+let info2:Info2={
+  data:"bye world"
+}
+
+info2={}
